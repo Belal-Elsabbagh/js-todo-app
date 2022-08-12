7d790491f33e1ad6c0efed24103b3411c52db841
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-08-13 00:19:12 +0200

    Minor refactoring

--- controllers/app-controller.js
+++ controllers/app-controller.js
@@ -1,28 +1,22 @@
 let models = require('../models/models');
 let urlencodedParser = require('body-parser').urlencoded({ extended: false })
 
-module.exports = function(app){
-    app.get('/todo',function(req, res) {
-        models.taskModel.find({}, function(err, taskDocs) {
-            let undoneTasks = taskDocs.filter(function (el) {
-                return el.isCompleted == false
-            });
-            let doneTasks = taskDocs.filter(function (el) {
-                return el.isCompleted == true
-            });
-            res.render('index', { 
-                undoneTasks: undoneTasks, 
-                doneTasks:doneTasks 
+module.exports = function (app) {
+    app.get('/todo', function (req, res) {
+        models.taskModel.find({}, function (err, taskDocs) {
+            res.render('index', {
+                undoneTasks: taskDocs.filter((i) => { return i.isCompleted == false }),
+                doneTasks: taskDocs.filter((i) => { return i.isCompleted == true })
             })
         })
     });
 
-    app.post('/todo', urlencodedParser, function(req, res) {
+    app.post('/todo', urlencodedParser, function (req, res) {
         let taskToBeAdded = req.body
         try {
-            if(!('task' in taskToBeAdded)) throw new Error("Wrong object format")
-            models.taskModel.create(taskToBeAdded, function(err, data) {
-                if(err) throw err; 
+            if (!('task' in taskToBeAdded)) throw new Error("Wrong object format")
+            models.taskModel.create(taskToBeAdded, function (err, data) {
+                if (err) throw err;
                 res.json(data)
             })
         }
@@ -31,46 +25,49 @@ module.exports = function(app){
         }
     });
 
-    app.post('/todo/complete', urlencodedParser, function(req, res) {
+    app.post('/todo/complete', urlencodedParser, function (req, res) {
         let taskToBeCompleted = req.body
-        try{
-            if(!('task' in taskToBeCompleted)) throw new Error("Wrong object format")
-            models.taskModel.updateOne(taskToBeCompleted, {isCompleted: true, timeCompleted: Date.now()}, function(err, docs){
-                if(err) throw (err)
-                res.json(docs)
-            })
+        try {
+            if (!('task' in taskToBeCompleted)) throw new Error("Wrong object format")
+            models.taskModel.updateOne(taskToBeCompleted,
+                { isCompleted: true, timeCompleted: Date.now() },
+                function (err, docs) {
+                    if (err) throw (err)
+                    res.json(docs)
+                })
         }
-        catch(err){
+        catch (err) {
             res.send(err)
         }
     });
 
-    app.post('/todo/reset', urlencodedParser, function(req, res) {
+    app.post('/todo/reset', urlencodedParser, function (req, res) {
         let taskToBeReset = req.body
-        try{
-            if(!('task' in taskToBeReset)) throw new Error("Wrong object format")
-            models.taskModel.updateOne(taskToBeReset, {isCompleted: false, timeCompleted: Date.now()}, function(err, docs){
-                if(err) throw (err)
+        try {
+            if (!('task' in taskToBeReset)) throw new Error("Wrong object format")
+            models.taskModel.updateOne(taskToBeReset, 
+                { isCompleted: false, timeCompleted: Date.now() }, 
+                function (err, docs) {
+                if (err) throw (err)
                 res.json(docs)
             })
         }
-        catch(err){
+        catch (err) {
             res.send(err)
         }
     });
 
-    app.post('/todo/delete',function(req, res) {
+    app.post('/todo/delete', function (req, res) {
         let taskToBeDeleted = req.body
-        try{
-            if(!('task' in taskToBeAdded)) throw new Error("Wrong object format")
-            models.taskModel.deleteOne(taskToBeDeleted, function(err, docs){
-                if(err) throw (err)
+        try {
+            if (!('task' in taskToBeAdded)) throw new Error("Wrong object format")
+            models.taskModel.deleteOne(taskToBeDeleted, function (err, docs) {
+                if (err) throw (err)
                 res.json(docs)
             })
         }
-        catch(err){
+        catch (err) {
             res.send(err)
         }
-
     });
 }
