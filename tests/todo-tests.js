e6766753127ac112d0fa2eb8bb3d1c4bce1b6817
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-08-17 15:58:04 +0200

    Generic test function

--- tests/todo-tests.js
+++ tests/todo-tests.js
@@ -1,20 +1,19 @@
 const axios = require('axios');
 const testId = '62f8e199cd12203206faca36'
-const TEST_getAllTodos = async () => {
-    let result = await axios.get('http://localhost:3000/user')
-    if (result.status === 200) return result.data
-    throw new 1
+const TEST = async (requestMethod, url, requestBody, successCode, errorCode) => {
+    const testResults = axios({
+        method: requestMethod,
+        url: url,
+        data: requestBody
+    });
+    if (testResults.status !== successCode) throw new errorCode
+    return testResults.data
 }
 
-const TEST_getTodoById = async (id) => {
-    let result = await axios.get(`http://localhost:3000/user/${id}`)
-    if (result.status === 200) return result.data
-    throw new 2
-}
-
-try{
-    TEST_getAllTodos()
-    TEST_getTodoById(testId)
+try {
+    TEST('get', 'http://localhost:3000/user', {}, 200, 1)
+    TEST('get', `http://localhost:3000/user/${testId}`, {}, 200, 2)
+    console.log("Tests passed")
     process.exit(0)
 } catch (err) {
     process.exit(err)
