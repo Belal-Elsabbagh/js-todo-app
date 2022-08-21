5ab138a8aec4d90a89029e6af62df8c3e2db45e8
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-08-21 18:17:55 +0200

    new test

--- test/app.test.js
+++ test/app.test.js
@@ -1,11 +1,10 @@
 const app = require("../app");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const expect = chai.expect;
-const end = chai.end
+const { expect } = chai;
 chai.use(chaiHttp);
 describe("Server!", () => {
-  it("get todos", done => {
+  it("get todos", (done) => {
     chai.request(app).get("/todo").end((err, res) => {
       expect(res).to.have.status(200);
       done();
