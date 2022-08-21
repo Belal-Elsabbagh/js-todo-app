109e28ca22b7d778b43c54811d0a30e813f0b8b9
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-08-21 18:34:24 +0200

    moved done

--- test/app.test.js
+++ test/app.test.js
@@ -7,9 +7,9 @@ describe("Server!", () => {
   it("get todos", (done) => {
     chai.request(app).get("/todo")
       .then((res) => {
-        expect(res.status).to.equal(200); done();
+        expect(res.status).to.equal(200);
       }).catch((err) => {
         throw err;
-      });
+      }).finally(done => { done() });
   });
 });
